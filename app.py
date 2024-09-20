5a06ce3931ca08031946028f09a0174c4f67cd94
Shad Spreiter <loyaldogtech@gmail.com>
2024-09-19 19:00:45 -0700

    Task 1 complete

---
+++ app.py
@@ -0,0 +1,19 @@
+def atm_menu(name):
+    print("")
+    print("          === Automated Teller Machine ===          ")
+    print("User: " + name)
+    print("------------------------------------------")
+    print("| 1.    Balance     | 2.    Deposit      |")
+    print("------------------------------------------")
+    print("------------------------------------------")
+    print("| 3.    Withdraw    | 4.    Logout       |")
+    print("------------------------------------------")
+
+
+print("    === Automated Teller Machine ===    ")
+
+name = input("Enter name to register: ")
+pin = input("Enter PIN: ")
+balance = 0
+
+print(name + " has been registered with a starting balance of $" + str(balance))
